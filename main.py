b85e64e447b08aa856b9566f2ba3d25105816fa8
ACA30 <ACA30@users.noreply.github.com>
2017-10-17 15:29:47 -0400

    Added more adjectives, colours, and nouns.

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import random
 import string
 #Password Generating Program
 #Name
-adjectives = ['sleepy', 'dopey', 'stupid', 'fat', 'old', 'ugly', 'awesome', 'egotistical', 'brave', 'kind', 'agreeable', 'living', 'dead', 'calm', 'delighted', 'colossal', 'tiny', 'bitter', 'delicious', 'fresh', 'abandoned', 'substantial', 'sparse', 'dank', 'moist', 'weak', 'wet', 'faint', 'hissing', 'puking', 'clever', 'rich', 'magnificent', 'glamorous', 'important', 'different', 'public', 'evil', 'aggressive', 'abiding', 'adhesive', 'alleged', 'addicted', 'acidic', 'helpful']
+adjectives = ['sleepy', 'dopey', 'stupid', 'fat', 'old', 'ugly', 'awesome', 'egotistical', 'brave', 'kind', 'agreeable', 'living', 'dead', 'calm', 'delighted', 'colossal', 'tiny', 'bitter', 'delicious', 'fresh', 'abandoned', 'substantial', 'sparse', 'dank', 'moist', 'weak', 'wet', 'faint', 'hissing', 'puking', 'clever', 'rich', 'magnificent', 'glamorous', 'important', 'different', 'public', 'evil', 'aggressive', 'abiding', 'adhesive', 'alleged', 'addicted', 'acidic', 'helpful', 'epic']
 colours = ['red', 'green', 'blue', 'purple', 'orange', 'pink', 'grey']
-nouns = ['banana', 'superhero', 'giraffe', 'dinosaur', 'pillow', 'cloud', 'keyboard', 'human', 'american', 'desk', 'coffee', 'store', 'plant', 'people', 'history', 'way', 'art', 'world', 'information', 'government', 'product', 'product', 'investment', 'technology', 'physics', 'analytics', 'policy', 'series', 'child', 'article', 'department', 'user', 'combination', 'disk', 'energy', 'student', 'wood', 'debt', 'prescription', 'secretary', 'alcohol', 'password', 'engineering']
+nouns = ['banana', 'superhero', 'giraffe', 'dinosaur', 'pillow', 'cloud', 'keyboard', 'human', 'american', 'desk', 'coffee', 'store', 'plant', 'people', 'history', 'way', 'art', 'world', 'information', 'government', 'product', 'product', 'investment', 'technology', 'physics', 'analytics', 'policy', 'series', 'child', 'article', 'department', 'user', 'combination', 'disk', 'energy', 'student', 'wood', 'debt', 'prescription', 'secretary', 'alcohol', 'password', 'engineering', 'chalk']
 
 name = input('What is your name? ')
 print('Welcome to the Secret Password Generator, ', name)
