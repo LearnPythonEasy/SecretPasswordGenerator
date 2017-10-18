8fb70b738bde29cfb2d782ea93b680e4902daddd
ACA30 <ACA30@users.noreply.github.com>
2017-10-18 14:08:30 -0400

    updated main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import random
 import string
+
 #Password Generating Program
-#Name
+#Created by Nate (http://github.com/ACA30)
+
 adjectives = ['sleepy', 'dopey', 'stupid', 'fat', 'old', 'ugly', 'awesome', 'egotistical', 'brave', 'kind', 'agreeable', 'living', 'dead', 'calm', 'delighted', 'colossal', 'tiny', 'bitter', 'delicious', 'fresh', 'abandoned', 'substantial', 'sparse', 'dank', 'moist', 'weak', 'wet', 'faint', 'hissing', 'puking', 'clever', 'rich', 'magnificent', 'glamorous', 'important', 'different', 'public', 'evil', 'aggressive', 'abiding', 'adhesive', 'alleged', 'addicted', 'acidic', 'helpful']
 colours = ['red', 'green', 'blue', 'purple', 'orange', 'pink', 'grey']
 nouns = ['banana', 'superhero', 'giraffe', 'dinosaur', 'pillow', 'cloud', 'keyboard', 'human', 'american', 'desk', 'coffee', 'store', 'plant', 'people', 'history', 'way', 'art', 'world', 'information', 'government', 'product', 'product', 'investment', 'technology', 'physics', 'analytics', 'policy', 'series', 'child', 'article', 'department', 'user', 'combination', 'disk', 'energy', 'student', 'wood', 'debt', 'prescription', 'secretary', 'alcohol', 'password', 'engineering']
 
 name = input('What is your name? ')
-if name == 'Nate':
-    print('')
-    print ('Hello, Nate! Welcome to the Super Top Secret Password Generator')
-else:
-    print('')
-    print('Welcome to the Secret Password Generator,', name)
+
+
+print('')
+print('Welcome to the Secret Password Generator,', name)
 
 while True:
     adjective = random.choice(adjectives)
